2a65512e7629f3a74c6819746ea7075f60f4960b
Alanna Celentano <celentanoad91@gmail.com>
2020-10-31 15:46:03 -0500

    add report web vitals file

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(publicPath));
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}!`);
- });
+app.listen(PORT);
